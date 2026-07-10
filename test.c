93538a7bd82bba44e04548b89e6369df3aa4e8de
Yuan Tian <67927023+tianyuan09@users.noreply.github.com>
2026-07-10 14:03:45 +0000

    Simplify programs for absolute beginners - remove advanced features like preprocessor directives, templates, and concepts

--- test.c
+++ test.c
@@ -12,26 +12,22 @@
  * TO COMPILE AND RUN:
  *   Method 1: Two separate commands
  *     FIRST, copy and paste this:
- *       gcc -std=c17 -o test_c test.c
+ *       gcc -o test_c test.c
  *     Press Enter. Wait for it to finish (you won't see output, that's normal).
  *     
  *     THEN, copy and paste this:
  *       ./test_c
- *     Press Enter. You should see: "Hello, C World! C17 is working..."
+ *     Press Enter. You should see: "Hello, C World!"
  * 
  *   Method 2: All in one command (easier!)
  *     Copy and paste this entire line into the terminal:
- *       gcc -std=c17 -o test_c test.c && ./test_c
+ *       gcc -o test_c test.c && ./test_c
  *     Press Enter. You should see the output appear!
  */
 
 #include <stdio.h>
 
 int main(void) {
-    #if __STDC_VERSION__ >= 201710L
-        printf("Hello, C World! C17 is working perfectly.\n");
-    #else
-        printf("Hello, C World! (Standard is lower than C17)\n");
-    #endif
+    printf("Hello, C World!\n");
     return 0;
 }
--- test.cpp
+++ test.cpp
@@ -1,5 +1,6 @@
 /*
  * C++ Learning Example: Modern C++20 Features
+ * Your First C++ Program!
  * 
  * INSTRUCTIONS FOR BEGINNERS:
  * 1. OPEN the TERMINAL (the black command line window)
@@ -11,33 +12,22 @@
  * TO COMPILE AND RUN:
  *   Method 1: Two separate commands
  *     FIRST, copy and paste this:
- *       g++ -std=c++20 -o test_cpp test.cpp
+ *       g++ -o test_cpp test.cpp
  *     Press Enter. Wait for it to finish (you won't see output, that's normal).
  *     
  *     THEN, copy and paste this:
  *       ./test_cpp
- *     Press Enter. You should see: "Hello, C++ World! C++20 features..."
+ *     Press Enter. You should see: "Hello, C++ World!"
  * 
  *   Method 2: All in one command (easier!)
  *     Copy and paste this entire line into the terminal:
- *       g++ -std=c++20 -o test_cpp test.cpp && ./test_cpp
+ *       g++ -o test_cpp test.cpp && ./test_cpp
  *     Press Enter. You should see the output appear!
  */
 
 #include <iostream>
-#include <concepts>
-
-template<typename T>
-concept Numeric = std::integral<T> || std::floating_point<T>;
-
-struct Config {
-    int version;
-    double scale;
-};
 
 int main() {
-    Config cfg{.version = 20, .scale = 1.0};
-    
-    std::cout << "Hello, C++ World! C++" << cfg.version << " features are working perfectly.\n";
+    std::cout << "Hello, C++ World!\n";
     return 0;
 }
